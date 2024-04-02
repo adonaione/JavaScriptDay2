9534e5d6f082266028a4c388d17c4f62d45289fb
adonaione <adonairomero@icloud.com>
2024-04-02 17:08:23 -0600

    done for day

--- main.js
+++ main.js
@@ -201,78 +201,397 @@
 ////////////
 
 // Declaring
-let people = ["Sri Yogananda", "Ram Das", "Sahdguru", "Buddha"];
-console.log(people);
-console.log(typeof people); // object
+// let people = ["Sri Yogananda", "Ram Das", "Sahdguru", "Buddha"];
+// console.log(people);
+// console.log(typeof people); // object
 
-// Accessing
-console.log(people[3])
+// // Accessing
+// console.log(people[3])
 
-console.log(people[-1]) // undefined - negative indices do not work 
+// console.log(people[-1]) // undefined - negative indices do not work 
 
-// Arrays have a built in .length property
-console.log(people.length); // 4
+// // Arrays have a built in .length property
+// console.log(people.length); // 4
 
-//////////////////
-// Array Methods//
-//////////////////
+// //////////////////
+// // Array Methods//
+// //////////////////
 
-// arr.push(value) - add value to end of the arr - returns new length
-console.log(people)
+// // arr.push(value) - add value to end of the arr - returns new length
+// console.log(people)
 
-let returnVal = people.push("Mary Magdaline");
-console.log(people);
-console.log(returnVal); // should be 5, because it returned the new length after adding
+// let returnVal = people.push("Mary Magdaline");
+// console.log(people);
+// console.log(returnVal); // should be 5, because it returned the new length after adding
 
-// arr.pop() - will remove the last element and return that element
-let popReturn = people.pop();
-console.log(people);
-console.log(popReturn); // Mary Magdaline returned
-
-
-const colors = ['red','orange', 'yellow', 'green', 'blue']
-colors.pop()
-colors.push('violet');
-console.log(colors);
-
-// arr.unshift(value) will add the value to the beginning and return new length
-console.log(people);
-
-let unshiftReturn = people.unshift("Master Osho");
-console.log(people);
-console.log(unshiftReturn);
+// // arr.pop() - will remove the last element and return that element
+// let popReturn = people.pop();
+// console.log(people);
+// console.log(popReturn); // Mary Magdaline returned
 
 
-//  arr.shift() removes the first item from an array and returns it
-let shiftReturn = people.shift();
-console.log(people);
-console.log(shiftReturn); //  Master Osho
+// const colors = ['red','orange', 'yellow', 'green', 'blue']
+// colors.pop()
+// colors.push('violet');
+// console.log(colors);
 
-////////////////////
-// Destructuring //
-//////////////////
+// // arr.unshift(value) will add the value to the beginning and return new length
+// console.log(people);
 
-// Rest Operator ...
-
-
-// Array Slicing
-
-// Array Splicing
+// let unshiftReturn = people.unshift("Master Osho");
+// console.log(people);
+// console.log(unshiftReturn);
 
 
-///////////////
-// Functions //
-///////////////
+// //  arr.shift() removes the first item from an array and returns it
+// let shiftReturn = people.shift();
+// console.log(people);
+// console.log(shiftReturn); //  Master Osho
 
+// // arr.indexOf9value) - will return the index of the value in the arr or -1 if not present
+// console.log(people.indexOf('Buddha'));
+// console.log(people.indexOf('Yoda')); // returns -1 bc not in list
 
-// Regular Functions
+// let nums = [10, 20, 15, 10, 12, 30, 20]
 
+// console.log(nums.indexOf(20));
+// console.log(nums.indexOf(20, 2));
 
-// Arrow Functions
+// // arr.forEach(callbackFn) - will execute the callback function on each element and discard the return value
+
+// people.forEach(function(element){console.log(element)});
+// people.forEach(function(person){console.log(`${person} is an enlightened being.`)});
 
 
 
-// The Spread Operator ...
+// // Destructuring 
+// // Array Deconstructing
+// // JS allows us to declare multiple cariables at a time and assign them memebers of the array
+
+// console.log(people);
+
+// let [yogi, psychonaut, guru, joy] = people;
+
+// console.log(yogi);
+// console.log(psychonaut);
+// console.log(guru);
+// console.log(joy);
+
+
+// let rainbow = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
+
+// let [r, o, y] = rainbow;
+
+// console.log(r, o, y);
+
+// let finalFour = ['UConn', 'Purdue', 'Alabama', 'NC State'];
+
+// [team1, team2, team3, team4, team5] = finalFour;
+
+// console.log(team1);
+// console.log(team2);
+// console.log(team3);
+// console.log(team4);
+// console.log(team5);
+
+// // Rest Operator ...
+// console.log(rainbow);
+// let [red, orange, yellow, ...others] = rainbow;
+
+// console.log(others);
+
+
+// // arr.join(string_to_join_on)  method joins all elements of an array into a string separated by commas or a specified separator
+// console.log(people.join(', '))
+
+// let things = ['a', 'b', 'c'];
+
+// console.log(things.join('-'));
+
+// // Array Slicing 
+// // Allows us to take a portion of the array by its indices. Original arr not modified
+// // arr.slice(start, end)
+
+// let months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun'];
+// console.log(months);
+// console.log(months.slice());
+// console.log(months.slice(3));  // From index 3 till end
+// console.log(months.slice(1, 4));    // From Index 1 till but not including 4
+// console.log(months.slice(-4)); //
+
+// // Array Splicing
+// // arr.splice(start, deleteCount, item1, item2, ..., itemN);
+
+// let fruits = ['apple', 'banana', 'peach', 'watermelon'];
+// console.log(fruits);
+
+// // Insert an item at an index position (assuming not beginning or end)
+// // To add 'pear' after banana in the fruits array
+// // start = 2
+// // deleteCount = 0
+// // item1 = 'pear'
+// fruits.splice(2, 0, 'pear');
+// console.log(fruits); // original array has been modified
+
+
+// // insert an item at index postion and remove item that was there **AKA replace**
+// // To replace 'peach' with 'orange'
+// // start = 3
+// // deleteCount = 1
+// // item1 = 'orange'
+// fruits.splice(3, 1, 'orange')
+// console.log(fruits);
+
+
+// // Insert and/or remove multiple items
+// // Replace 'banana' 'pear' and 'orange' with 'kiwi', 'grapes', and 'mango'
+
+// fruits.splice(1, 3, 'kiwi', 'grapes', 'mango');
+// console.log(fruits);
+
+// fruits.splice(3, 1, 'dragon fruit', 'lady finger bananas');
+// console.log(fruits);
+
+
+// /*
+//     Make an Array of your favorite foods (minimum 4). Then using the methods we have learned, add "Fried Tarantulas" to the end of the array, then add "Mokh Mchermel" to the front. Then, using a slice of the now modified array, print your original list of favorite foods without my additions.
+// */
+// let favFoods = ['yogi bowl', 'curry', 'quinoa', 'kitchari']
+// console.log(favFoods);
+// favFoods.push(('Fried Tarantulas'));
+// console.log(favFoods);
+// favFoods.unshift('Mokh Mchermel');
+// console.log(favFoods);
+// console.log(favFoods.slice(1, 5));
+
+// ///////////////
+// // Functions //
+// ///////////////
+
+
+// // Regular Functions
+
+// /* 
+// Syntax:
+
+// function: funcName(param1, param2, etc.){
+//     code to run when executed
+// }
+// */
+
+// function addNums(){
+//     let num1 = 10;
+//     let num2 = 20;
+//     return num1 + num2;
+// }
+
+// console.log(addNums);
+// console.log(typeof addNums);
+
+// // to execute - funcName() OR funcName(arg1, arg2, etc)
+// console.log(addNums());
+
+
+// // Using Parameters
+// function addNumsAgain(num1, num2){
+//     return num1 + num2;
+// };
+
+// console.log(addNumsAgain); // returns function
+
+// console.log(addNumsAgain(16, 28)); // returns 44
+
+// console.log(addNumsAgain(15)); // returns NaN
+
+
+// function logArgs(a, b){
+//     console.log('A is', a, typeof a)
+//     console.log('B is', b, typeof b)
+// }
+
+// logArgs(22); // A is number 22, B is undefined **NO ERROR**
+
+// // logArgs(b=13, a='hello') /// keyword Arguments are non-existent in JS
+
+
+// /*
+// Create a JS Function that takes a first name and last name and returns a String that says Hello first name last name. The input would be something like:
+
+// "Bo", "Jackson"
+
+// and the output would be:
+
+// "Hello Bo Jackson"
+// */
+
+// function greeting(fName, lName){
+//     return  `Hello ${fName} ${lName}`
+// }
+
+// console.log(greeting('Bo', 'Jackson'))
+// console.log(greeting('Michael', 'Jackson'))
+
+
+
+// // Arrow Functions
+
+// // The most basic arrow function
+
+// let doNothing = () => {};
+// console.log(doNothing);
+// console.log(typeof doNothing);
+
+// /*
+// Syntax
+// const funcName = (param1, param2) => {
+//     //do stuff
+//     return value
+// }
+
+
+// * if only one parameter, the parentheses are optional
+// const funcName = param1 => {
+//     // do stuff
+//     return value
+// }
+
+
+// * if your function is one line returning a value, you can use the implicit return
+// const funcName = (param1, param2) => returnValue
+
+// */
+
+
+// const addNums2 = (num1, num2) => {
+//     num1 += 10;
+//     num2 += 10;
+//     return num1 + num2;
+// }
+
+// console.log(addNums2);
+// console.log(typeof addNums2);
+
+// console.log(addNums2(4, 8))
+
+// // with one param
+// const squareNum = num => {
+//     return num ** 2
+// }
+
+// console.log(squareNum);
+// console.log(typeof squareNum);
+
+// console.log(squareNum(10));
+// console.log(squareNum(4));
+
+
+// // Implicit Return
+// const squareNum2 = num => num ** 2;
+
+// console.log(squareNum2);
+// console.log(typeof squareNum2);
+
+// console.log(squareNum2(10));
+// console.log(squareNum2(4));
+
+
+// // The Spread Operator ...
+
+// // ... As a rest operator
+// // let [x, y, ...z] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// // console.log(x);
+// // console.log(y);
+// // console.log(z);
+
+
+// function useSpread(x, y, z){
+//     console.log('x:', x);
+//     console.log('y:', y);
+//     console.log('z:', z);
+//     return x * y + z
+// };
+
+// let myNumArr = [11, 2, 7];
+
+// // traditionally we can call our function like this:
+// let myNewNum = useSpread(11, 2, 7);
+// console.log(myNewNum);
+
+// // Or like this
+// let myNewNum2 = useSpread(myNumArr[0], myNumArr[1], myNumArr[2]);
+// console.log(myNewNum2);
+
+
+// // using the spread operator
+// let myNewNum3 = useSpread(...myNumArr);
+// console.log(myNewNum3);
+
+
+// // We can also use the spread operator when making copies of the array
+// let myNumArray2 = [...myNumArr];
+// console.log(myNumArray2);
+
+// let myNumArr3 = ['hello', ...myNumArr, 'goodbye'];
+// console.log(myNumArr3);
+
+
+// // Array Methods with callback functions!
+
+// // Array.forEach(callbackFn)   -> callbackFn(element, index, arr)
+
+let baseballTeams = ['White Sox', 'Red Sox', 'Tigers', 'Cardinals', 'Mets', 'Dodgers', 'Giants'];
+
+// function myCallBack(element, index, arr){
+//     console.log('Element:', element);
+//     console.log('Index:', index);
+//     console.log('Array:', arr);
+//     return 10
+// }
+
+// baseballTeams.forEach(myCallBack);
+
+// baseballTeams.forEach( e => console.log(`I like to watch the ${e} play baseball`))
+
+// baseballTeams.forEach( (e, i) => console.log(`${e} is at index ${i}`))
+
+// // Array.map(callbackFn) -- callbackFn (element, index, arr)
+// // return a new array with the return values of callbackFn on each element
+
+let teamLengths = baseballTeams.map( team => team.length );
+console.log(teamLengths);
+
+let teamIndex = baseballTeams.map( (el, idx) => el + idx );
+console.log(teamIndex);
+
+// // Array.filter(callbackFn) - callackFN(element.=, index, arr)
+// // returns a new array of elements that pass the test implemented by the provided function
+
+// function isMultiWords(teamName){
+//     let teamWords = teamName.split(' ')
+//     return teamWords.length >= 2
+// }
+
+// let multiMap = baseballTeams.map(isMultiWords);
+// console.log(multiMap);
+
+// let multiFilter = baseballTeams.filter(isMultiWords); console.log(multiFilter)
+
+
+// // Array.reduce(callbackFn) - callbackFn(accumulator, currentValue)
+
+// let numbers = [2, 4, 7, 9, 6, 1, 5];
+
+// function reduceCallback(acc, current){
+//     console.log('Accumulator', acc);
+//     console.log('Current Value', current);
+//     console.log('Returns', acc + current);
+//     console.log('------------------------------------------------')
+//     return acc + current
+// };
+
+// let total = numbers.reduce(reduceCallback);
+// console.log(total);
+
 
 //////////////////
 // Flow Control //
@@ -280,17 +599,129 @@ console.log(shiftReturn); //  Master Osho
 
 // Review ifs
 
+let myAge = 25;
+
+let eligibility;
+if (myAge > 35){
+    eligibility = true;
+} else {
+    eligibility = false
+};
+
+console.log(eligibility);
+
 // Ternary Operator
+// boolean or condition ? actionIfTrue : actionIfFalse
+let val1 = 20;
+let val2 = 15;
+
+let val3 = (val1 > val2) ? 'Hello' : 'Goodbye';
+console.log(val3);
+
+
+let birthYear = 1999;
+
+let myGeneration;
+if (birthYear >= 1946 && birthYear <= 1964){
+    myGeneration = 'Boomer'
+} else if (birthYear <= 1979){
+    myGeneration = 'Gen X'
+} else if (birthYear <= 1995){
+    myGeneration = 'Millenial'
+} else {
+    myGeneration = 'Zoomer'
+};
+
+console.log(myGeneration);
+
+let myGeneration2 = (birthYear <= 1964) ? 'Boomer' : (birthYear <= 1979) ? 'Gen X' : (birthYear <= 1995) ? 'Millenial' : 'Zoomer';
+console.log(myGeneration2);
 
 // Traditions For Loop
+// for (varName=startingValue; stopCondition; varName increment/decement)
+
+// Simple Example
+for (let i=0; i< 5; i++){
+    console.log(i)
+};
+
+console.log('----------')
+
+for (let j=10; j <= 100; j+=5){
+    console.log(j);
+}
+
+console.log('----------')
+
+for (let k=10; k>0; k--){
+    console.log(k);
+};
+
+// use traditional for loop to loop over an array
+// start = 0 to represent index
+// stopCondtion < arr.length
+// increment by 1
+
+console.log(baseballTeams);
+
+for (let idx=0; idx < baseballTeams.length; idx++){
+    console.log(baseballTeams[idx])
+}
 
 // For...of Loop
+// Loop over an iterable giving each iterable item
+// Syntax:  for (let element of iterable){ do something for each }
+
+for (let team of baseballTeams){
+    console.log(team);
+}
+
+// ---works with string as well
+let firstPresident = 'George Washington';
+
+for (let chr of firstPresident){
+    console.log(chr)
+};
 
 // For...in loop
+// Will loop thorugh the properties of an opbject 
+// an Array is a special type of object where the indices are properties
+
+for (let i in baseballTeams){
+    console.log(i);
+}
+
+let exObj = {firstName: 'Brian', lastName: 'Stanton', city: 'Chicago'};
+
+for (let key in exObj){
+    console.log(key);
+};
 
 // while loop
+// Sytnax :
+// while(condition){ code to run while condition is true }
+
+let randomNumber = Math.floor(Math.random() * 10);
+console.log('start', randomNumber);
+
+while (randomNumber != 5){
+    randomNumber = Math.floor(Math.random() * 10);
+    console.log(randomNumber);
+}
+
+console.log('end', randomNumber);
 
 // do...while loop
+// Syntax:
+// do { statement } while (condition);
+// The do is run AT LEAST one time
+
+let myNum = 10;
+
+do {
+    myNum += 10;
+    console.log(myNum);
+} while (myNum <= 100);
 
 
 // Switch Case
